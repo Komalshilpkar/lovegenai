942c859b70f661f8af8e95d2d2e5b15081d4a523
Nandini Shilpkar <YOUR_EMAIL@gmail.com>
2026-02-11 16:47:08 +0530

    added supabase keys

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "wqdkvdnxxaqgynypwnee.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZGt2ZG54eGFxZ3lueXB3bmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3OTMxNTksImV4cCI6MjA4NjM2OTE1OX0.zTMgJCoHEbjpfrO6tvo6w4JfNTa06vw4W04EstAJ_KA";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
